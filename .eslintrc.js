883ad880c9483fa4b229085e1efb5da678efd007
mattdean1 <matt.dean.001@gmail.com>
2019-08-04 19:51:04 +0100

    Better config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,16 @@
-module.exports =  {
-    parser:  '@typescript-eslint/parser',  
-    extends:  [
-      'plugin:@typescript-eslint/recommended', 
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: ["plugin:@typescript-eslint/recommended"],
+  parserOptions: {
+    ecmaVersion: 2019,
+    sourceType: "module"
+  },
+  rules: {
+    semi: ["error", "never"],
+    "@typescript-eslint/explicit-function-return-type": [
+      "error",
+      { allowExpressions: true }
     ],
-   parserOptions:  {
-      ecmaVersion:  2018,
-      sourceType:  'module', 
-    },
-    rules:  {
-      semi: ["error", "never"]
-    },
-  };
+    "@typescript-eslint/no-use-before-define": 0
+  }
+}
