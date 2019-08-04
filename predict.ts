df06895c52aaedb2d0540c7f46eac771702a0d8e
mattdean1 <matt.dean.001@gmail.com>
2019-08-04 18:10:22 +0100

    Add Trie type with insert

--- predict.ts
+++ predict.ts
@@ -1,3 +1,51 @@
-const x: string = 9
+interface ChildNodes {[key: string]: TrieNode}
 
-console.log('toto')
+
+interface TrieNode {
+    letter: string;
+    children: ChildNodes;
+    isLeaf: boolean;
+    weight?: number;
+}
+
+class TrieNode {
+    public constructor(letter: string) {
+        this.letter = letter
+        this.children = {}
+        this.isLeaf = false
+    }
+}
+
+const insertWord = (node: TrieNode, word: string): void => {
+    if (word.length === 0) {
+        if (node.isLeaf) node.weight++
+        else {
+            node.isLeaf = true
+            node.weight = 0
+        }
+    } else {
+        const first = word.charAt(0)
+        if (!node.children[first]) {
+            node.children[first] = new TrieNode(first)
+        }
+        insertWord(node.children[first], word.slice(1))
+    }
+}
+
+class Trie extends TrieNode {
+    public constructor() {
+        super(null)
+    }
+
+    public insert(word: string): void {
+        insertWord(this, word)
+    }
+}
+
+
+const PredictionTrie = new Trie()
+
+const words = ["a", "aadvark", "animal", "anteater", "and"]
+words.forEach(word => PredictionTrie.insert(word))
+
+console.log(JSON.stringify(PredictionTrie, null, 2))
